8504b38c19a3437bcad58b93cb8e18c7f58fce13
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-04 01:14:24 +0800

    feat: integrate nodemailer for email verification instead of Resend

--- backend/resend/email.js
+++ backend/resend/email.js
@@ -1,20 +1,32 @@
-import { resend } from "./config.js";
+import nodemailer from "nodemailer";
 import { emailVerifTOkenTemplate } from "./emailTemplate.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "geraldberongoy04@gmail.com", // Your Gmail address
+    pass: process.env.GMAIL_PASSWORD, // Use the generated App Password, NOT your actual Gmail password
+  },
+});
 
 export const sendVerificationEmail = async (email, verificationToken) => {
   try {
-    const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: [email],
+    const mailOptions = {
+      from: "geraldberongoy04@gmail.com", // Replace with your Gmail
+      to: email,
       subject: "Verify Your Email Address",
       html: emailVerifTOkenTemplate.replace(
         "{verificationToken}",
         verificationToken
       ),
-    });
-    console.log("Email sent: ", data);
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: ", info.response);
   } catch (error) {
-    console.error("Error sending verif email: ", error);
+    console.error("Error sending verification email: ", error);
     throw new Error("Error sending verification email");
   }
 };
