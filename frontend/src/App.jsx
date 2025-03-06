b2a429ea62953774a470807b03123a400a68ac0e
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-06 15:54:22 +0800

    feat: add route for email verification page in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
 import { Toaster } from "react-hot-toast";
+import VerifyEmail from "../pages/VerifyEmail";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/signup" element={<SignupPage />} />\
+          <Route path="/verify-email" element={<VerifyEmail />} />
         </Routes>
       </div>
     </>
