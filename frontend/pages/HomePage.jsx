2ec34a009e9261cf52c75dc45ecc0f61238f3e47
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-06 22:51:29 +0800

    feat: enhance HomePage layout with hero section and improved button styles

--- frontend/pages/HomePage.jsx
+++ frontend/pages/HomePage.jsx
@@ -3,13 +3,25 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <Link to={"/signup"}>
-        <button className="btn">Signup</button>
-      </Link>
-      <Link to={"/login"}>
-        <button className="btn">Login</button>
-      </Link>
+    <div class="hero bg-base-200 min-h-screen">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Hello there</h1>
+          <p class="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <div class="flex justify-center gap-4">
+            <Link to={"/signup"}>
+              <button className="btn btn-primary">Signup</button>
+            </Link>
+            <Link to={"/login"}>
+              <button className="btn btn-primary">Login</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
