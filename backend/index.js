960d81d8dd3674012c42e3d567c3b128f98fd0db
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-07 12:46:24 +0800

    feat: enhance CORS configuration to allow specific origins

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,25 @@ const app = express();
 //middleware json
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+const cors = require("cors");
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-project-with-auth.vercel.app/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 
