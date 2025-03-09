10025ea68713a17c227d18eefeb7bb1a29536d86
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-09 17:10:10 +0800

    feat: update JWT token cookie settings to allow cross-site requests

--- backend/utils/generateJWTToken.js
+++ backend/utils/generateJWTToken.js
@@ -8,7 +8,7 @@ export const generateJWTToken = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
