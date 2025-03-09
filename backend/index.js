9b1e64300c9fbe4ef01b1acc7793be0f400c2cfd
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-09 16:48:57 +0800

    feat: simplify CORS configuration to allow all origins

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, origin);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     credentials: true,
   })
 );
